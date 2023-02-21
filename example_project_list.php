26226229a012c10a3a1159419519fb028283ed9c
Vít Michalek <info@semor.cz>
2023-02-21 10:58:05 +0100

    update result

--- example_project_list.php
+++ example_project_list.php
@@ -4,10 +4,12 @@ include "semor_api.php";
 
 SEMOR::$token = "XXX";
 
-
 $result = SEMOR::GetProjectList();
-if($result["status"] == 200){//zalozen novy projekt
-	foreach($result["list"] as $project){
+if($result["status"] == 200){//
+	foreach($result["server"]["active"] as $project){
+
+	}
+	foreach($result["server"]["inactive"] as $project){
 
 	}
 }else{
